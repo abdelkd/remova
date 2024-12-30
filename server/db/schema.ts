9e85c8444e77c5334c7cdc3f84718b48fa71c9a4
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-30 14:14:28 +0100

    add bucket ids

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,12 +1,14 @@
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 import type { InferSelectModel } from 'drizzle-orm';
+import { randomUUID } from 'node:crypto';
 
 export const userTable = sqliteTable('user', {
   id: integer('id').primaryKey(),
   email: text('email').notNull(),
   password: text('password').notNull(),
   creditsLeft: integer('credits_left').notNull().default(0),
+  bucketId: text('bucket_id').notNull().$defaultFn(randomUUID),
 });
 
 export const sessionTable = sqliteTable('session', {
--- server/db/utils.ts
+++ server/db/utils.ts
@@ -1,12 +1,14 @@
 import { db, userTable } from '@/server/db';
 import { eq } from 'drizzle-orm';
+import { randomUUID } from 'node:crypto';
 
 export const getUserByEmail = (email: string) => {
   return db.select().from(userTable).where(eq(userTable.email, email));
 };
 
 export const registerNewUser = (email: string, password: string) => {
-  return db.insert(userTable).values({ email, password });
+  const bucketId = randomUUID();
+  return db.insert(userTable).values({ email, password, bucketId });
 };
 
 export const getUserCredits = async (id: number) => {
