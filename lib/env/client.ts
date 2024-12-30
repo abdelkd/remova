52ad1787708d1045f3e0b55aafd68dc34608e215
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-30 13:46:06 +0100

    feat: add supabase

--- lib/env/client.ts
+++ lib/env/client.ts
@@ -1,7 +1,11 @@
 import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
 
 export const env = createEnv({
   clientPrefix: 'PUBLIC_',
-  client: {},
+  client: {
+    PUBLIC_SUPABASE_URL: z.string().url(),
+    PUBLIC_SUPABASE_KEY: z.string().min(1),
+  },
   runtimeEnv: process.env,
 });
