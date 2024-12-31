ee6f471e77396de02c14e9f2d2351479a1a83b19
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-31 15:41:57 +0100

    fix server env error

--- server/actions/index.ts
+++ server/actions/index.ts
@@ -10,15 +10,12 @@ import { randomUUID } from 'node:crypto';
 
 export const loginUser = async ({ email, password }: AuthForm) => {
   const supabase = createClient(await cookies());
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
   console.log({ data, error });
-  if (!user || error) {
+  if (!data.user || error) {
     console.error(error);
     return { data, error };
     // redirect('/error');
@@ -34,17 +31,14 @@ export const loginUser = async ({ email, password }: AuthForm) => {
 export const signUpUser = async ({ email, password }: AuthForm) => {
   const supabase = createClient(await cookies());
   try {
-    const {
-      data: { user },
-      error,
-    } = await supabase.auth.signUp({ email, password });
-    if (!user || error) {
+    const { data, error } = await supabase.auth.signUp({ email, password });
+    if (!data.user || error) {
       console.error(error);
       return { data, error };
     }
 
     const bucketId = randomUUID();
-    await registerNewUser({ id: user.id, email, password, bucketId });
+    await registerNewUser({ id: data.user.id, email, password, bucketId });
     return { data, error };
   } catch (err) {
     console.log(err);
